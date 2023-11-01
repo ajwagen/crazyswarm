8a266421ce42b1b1d5ceecbc1615bea8a1c1c42f
rwik2000 <rwik2000@gmail.com>
2023-11-01 14:22:20 -0700

    feedback linearization log

--- ros_ws/src/crazyswarm/scripts/run_SysID/Controllers/ctrl_backbone.py
+++ ros_ws/src/crazyswarm/scripts/run_SysID/Controllers/ctrl_backbone.py
@@ -162,21 +162,70 @@ class ControllerBackbone():
         param.sim_dt = 0.02
         param.dt = 0.02
 
-        kernel = FixedLearnedKernel()
+        kernel = FixedLearnedKernel(weights_name='weights_checkpoint_xy2')
         # Aker = np.load(self.exploration_dir+'aker.npy')
         aker = torch.tensor(0.01*np.random.randn(3,3), dtype=torch.float32)
         controller = QuadrotorPIDController(torch.tensor([6, 4, 1.5], dtype=torch.float32), param, Adrag=torch.zeros(3,12), optimize_Adrag=True, control_angvel=True, kernel=kernel)
         # controller_params = np.load('/home/rwik/proj/Drones/icra_23/Opt_Nonlinear_SysID_Quad/Opt_Nonlinear_SysID_Quad/data/explore_circle_aggressive_opt_controller.npz', allow_pickle=True)
         # controller_params = np.load('/home/rwik/proj/Drones/icra_23/Opt_Nonlinear_SysID_Quad/Opt_Nonlinear_SysID_Quad/data/random_plate_0_policy_params.npz', allow_pickle=True)
         # gains = torch.tensor([8.9898, 8.2800, 3.7587], dtype=torch.float)
-        gains = torch.tensor([ 3.9785,  7.4346, -0.1567])
-        aker = torch.tensor([[ 0.0333,  0.0672,  0.0859,  0.0841, -0.1125, -0.3306,  0.1373,  0.0065,
-         -0.1780, -0.0017, -0.0149,  0.0704],
-        [-0.0758,  0.0009, -0.0571, -0.0469, -0.0630, -0.3979,  0.2740, -0.0286,
-         -0.6609,  0.1521,  0.0013, -0.0346],
-        [-0.2763, -0.0192, -0.3056, -0.3024, -0.0485, -0.0668,  0.0256,  0.3047,
-         -0.0843,  0.0319,  0.1334, -0.3213]])
+        gains = torch.tensor([ 6.0,  4.0, 1.0])
+        aker = torch.tensor([[ 0.6939, -0.4696, -0.5149, -1.0297, -1.5843,  0.4077,  0.3526,  0.1648,
+         -0.2010,  0.0802, -0.0952, -0.1213],
+        [ 2.8421, -1.3046, -4.3308, -3.7894, -1.1156, -1.4041,  0.2328, -0.2722,
+         -0.0615, -0.0452, -0.0235, -0.0657],
+        [ 1.4143, -0.3964,  0.4221, -0.0520,  0.2544, -0.6137,  0.0108,  3.3282,
+         -0.6375,  0.1638,  0.1868, -1.4380]])
         controller.update_params([gains, aker])
+
+        if False:
+            from Opt_Nonlinear_SysID_Quad.environments import QuadrotorAirDrag
+            from Opt_Nonlinear_SysID_Quad.crazyflie_helper import load_cf_data, convertDict2Array
+            from Opt_Nonlinear_SysID_Quad.param_torch import Param as Param_sysid
+            files = ['ch_poly_seed3_plate','circle_r0.5p2.0_plate_0','ch_poly_seed0_plate','ch_poly_seed4_plate', \
+            'ch_poly_seed2_plate','circle_r0.3p3.0_plate_0','ch_poly_seed1_plate','circle_r0.5p4.0_plate_0']
+            class args_:
+                takeofftime = 4.2
+                bh = 0.6 # change to 0.6 if using with plate and fan
+                runtime = 10.0
+                hovertime = 4.0
+            states = []
+            inputs = []
+            for file_id in files:
+                fileNames = ['./data/' + file_id + '.npz']
+                # args = parser.parse_args()
+                # args.takeofftime = 4.2
+                # args.bh = 0.6 # change to 0.6 if using with plate and fan
+                # args.runtime = 10.0
+                # args.hovertime = 4.0
+                args = args_()
+                data_dict = load_cf_data(fileNames, args)
+                states2, inputs2 = convertDict2Array(data_dict, mass=1)
+                states.append(states2[0])
+                inputs.append(inputs2[0])
+
+
+            for i in range(len(states)):
+                filter_cutoff = int(0.07 * len(states[i]))
+                states_fft = torch.fft.fft(states[i], dim=0)
+                states_fft[filter_cutoff:-filter_cutoff+1,:] = 0
+                states[i] = torch.real(torch.fft.ifft(states_fft, dim=0))
+
+                inputs_fft = torch.fft.fft(inputs[i], dim=0)
+                inputs_fft[filter_cutoff:-filter_cutoff+1,:] = 0
+                inputs[i] = torch.real(torch.fft.ifft(inputs_fft, dim=0))
+
+            # param_instance = Param_s(config, MPPI=True)
+            H = 500
+            H_goal = 500
+            Aker = torch.zeros(3,12)
+            true_instance = QuadrotorAirDrag(param, config, H, Aker, kernel, maximize=True, H_goal=H_goal, k_delay=1.0)
+            true_instance.update_parameter_estimates(states,inputs,action_delay=True,regularizer=1)
+            aest = true_instance.get_dynamics().detach()
+            controller.update_params([(1/0.02)*aest])
+
+            exit()
+     
         return controller
 
     def _response(self, fl1, response_inputs):
